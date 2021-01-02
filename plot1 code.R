475687662067e30b0a01f3ccd15766185cee7d9d
chefhousgit <ajinemahmoud@gmail.com>
2021-01-01 20:11:53 -0600

    Created code for plot1.png

---
+++ plot1 code.R
@@ -0,0 +1,7 @@
+
+data <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+date <- as.Date(data$Date, "%d/%m/%Y")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+twodays <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02",]
+twodays <- twodays[complete.cases(twodays),]
+hist(twodays$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
