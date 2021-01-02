531a82beeeeaa8a2954ba870923c46fb8488fc3d
chefhousgit <ajinemahmoud@gmail.com>
2021-01-01 20:26:34 -0600

    Created code for plot1.png

--- plot1 code.R
+++ plot1 code.R
@@ -4,4 +4,7 @@ date <- as.Date(data$Date, "%d/%m/%Y")
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 twodays <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02",]
 twodays <- twodays[complete.cases(twodays),]
+png(filename = "plot1.png", width = 480, height = 480)
 hist(twodays$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+while (!is.null(dev.list()))
+    dev.off()
